dc11b955876165e42e509b7fff258a0e93ee70cc
xsfunc <xsfunc@gmail.com>
2023-09-08 20:18:29 +0300

    feat: add icons plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import swc from 'unplugin-swc'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    Icons({
+      autoInstall: true,
+      compiler: 'jsx',
+      jsx: 'react',
+    }),
     swc.vite({
       jsc: {
         experimental: {
