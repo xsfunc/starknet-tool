8253b87b0f39a4f543f4de400a774b4c7caa77b0
xsfunc <xsfunc@gmail.com>
2023-09-10 15:16:31 +0300

    fix: eslint

--- src/shared/lib/modal-factory/index.ts
+++ src/shared/lib/modal-factory/index.ts
@@ -1,15 +1,5 @@
-import type { Event, Store } from 'effector'
 import { createEvent, createStore } from 'effector'
 import { createFactory } from '@withease/factories'
-
-interface ModalState extends Record<string, Store<any> | Event<any>> {
-  isOpen: Store<boolean>
-  close: Event<void>
-  open: Event<void>
-}
-
-type UnitObject = Record<string, Store<any> | Event<any>>
-// <T extends UnitObject>(units: T): ModalState & T
 
 export const createModal = createFactory(<T>() => {
   const closeCalled = createEvent()
