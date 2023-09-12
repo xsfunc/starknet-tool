673b5ad20d8236d1b580c8238ab44cec5c996aea
xsfunc <xsfunc@gmail.com>
2023-09-12 11:29:09 +0300

    feat: okx add balances query

--- src/shared/lib/okx/api.ts
+++ src/shared/lib/okx/api.ts
@@ -1,42 +1,50 @@
 import { createMutation } from '@farfetched/core'
-import { getSignature } from './methods'
-import type { Withdrawal } from './types'
+import { authHeaders } from './methods'
+import type { Credentials, Withdrawal } from './types'
 
 const baseUrl = 'https://www.okx.com'
 
 export const withdrawMutation = createMutation({
   handler: async ({ credentials, ...body }: Withdrawal) => {
+    const method = 'POST'
     const endpoint = '/api/v5/asset/withdrawal'
-    const { signature, timestamp } = await getSignature({
-      apiSecret: credentials.apiSecret,
-      method: 'POST',
+    const { headers } = await authHeaders({
+      credentials,
       endpoint,
-      body,
+      method,
     })
 
     const response = await fetch(
       `${baseUrl}${endpoint}`,
       {
-        method: 'POST',
-        headers: {
-          'OK-ACCESS-KEY': credentials.apiKey,
-          'OK-ACCESS-PASSPHRASE': credentials.apiPassphrase,
-          'OK-ACCESS-SIGN': signature,
-          'OK-ACCESS-TIMESTAMP': timestamp,
-          'Content-Type': 'application/json',
-        },
+        method,
+        headers,
         body: JSON.stringify(body),
       },
     )
-      .catch((e) => {
-        console.error(e)
-        return e
-      })
+    return response.json()
+  },
+})
 
+export const balancesQuery = createMutation({
+  handler: async (credentials: Credentials) => {
+    const method = 'GET'
+    const endpoint = '/api/v5/asset/balances?ccy=ETH'
+    const { headers } = await authHeaders({
+      credentials,
+      endpoint,
+      method,
+    })
+
+    const response = await fetch(
+      `${baseUrl}${endpoint}`,
+      { method, headers },
+    )
     return response.json()
   },
 })
 
 export const api = {
   withdraw: withdrawMutation,
+  balances: balancesQuery,
 }
